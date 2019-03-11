5fc12d57514fa59692921d592f18e98dcb517664
ChaosKai <kai-waggeling@web.de>
2019-03-11 23:38:41 +0100

    Create master.user.js

---
+++ userscript/master.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name        Fahrzeugtableau
+// @namespace   Leitstellenspiel
+// @description Export der Wachen & Fahrzeuge aus dem LSS zum Fahrzeugtableau
+// @downloadURL https://rawgit.com/ChaosKai/fahrzeugtableau-standalone/master/userscript/master.user.js
+// @include     http*://www.leitstellenspiel.de/*
+// @version     dev
+// @author      ChaosKai93
+// @grant       none
+// ==/UserScript==
+
+var scriptElement = document.createElement("script");
+scriptElement.type = "text/javascript";
+scriptElement.src = "https://rawgit.com/ChaosKai/fahrzeugtableau-standalone/master/userscript/content.js";
+document.body.appendChild(scriptElement);
